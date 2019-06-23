c7fe2c1d7a12836988f3fcf9960c9b3978f2cbba
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-23 11:51:01 -0300

    Configuração inicial do arquivo server (start da api) e instalação dos módulos básicos, como: express, body-parser, connect-multiparty, mongodb etc.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const objectId = require('mongodb').ObjectID;
+const multiparty = require('connect-multiparty');
+// manipulação de arquivos
+const mv = require('mv'); // necessário para linux
+const fs = require('fs'); // nativo do nodejs
+
+// start do express;
+const aplication = express();
+
+// middlwares
+aplication.use(bodyParser.urlencoded({ extended: true }));
+aplication.use(bodyParser.json());
+aplication.use(multiparty());
+
+// definição da porta
+const PORT = 8080;
+
+aplication.listen(PORT, () => console.log('API no ar...'));
