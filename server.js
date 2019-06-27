8537e05a3092b6317c3d2b670f00214dd4ba4339
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-27 17:43:29 -0300

    Refresh do start da app e add de novas dependências do projeto.

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ const aplication = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const multiparty = require('connect-multiparty');
-const cors = require('cors'); // permissões
+const cors = require('cors'); // permissões p/ consumo da api
 const consign = require('consign');
+const fileUpload = require('express-fileupload');
+const multer = require('multer');
 // manipulação de arquivos
 const mv = require('mv'); // necessário para linux
-const fs = require('fs'); // nativo do nodejs
+const fs = require('fs');
 
-aplication.use(bodyParser.json())
-aplication.use(bodyParser.urlencoded({ extended: false }));
+
+const vdc = multiparty();
+aplication.use(multiparty())
+aplication.use(fileUpload());
+
+
+aplication.use(express.json({ extend: false }));
+aplication.use(express.urlencoded({ extended: false }));
+
 
 // Configurando banco de dados
 const dbConfig = require('./src/config/database.js');
-
-const User = require('./src/models/user.model.js');
 
 mongoose.Promise = global.Promise;
 
@@ -29,12 +36,27 @@ mongoose.connect(dbConfig.url, { useNewUrlParser: true })
     });
 // permissões
 const corsOptions = {
-    origin: '*', // client
+    origin: '*', // client (todo mundo pode acessar)
     optionsSuccessStatus: 200
 }
 
 aplication.use(cors(corsOptions));
 
-consign().include('./src/routes').into(aplication);
+consign()
+    .include('./src/routes')
+    .into(aplication);
+
+/*
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './src/uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.ogirinalname)
+    }
+});
+*/
 
 aplication.listen(3000, () => console.log('APP on...'));
